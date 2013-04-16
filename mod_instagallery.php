8e0be164fb206d05f652852331a437129abe7724
Daniel Eliasson <daniel@stilero.com>
2013-04-16 14:01:12 +0200

    first commit

---
+++ mod_instagallery.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @version SVN: $Id: mod_#module#.php 96 2011-08-11 06:59:32Z michel $
+ * @package    Instagram
+ * @subpackage Base
+ * @author     Daniel Eliasson Stilero Webdesign
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+defined('_JEXEC') or die('Restricted access'); // no direct access
+
+require_once (dirname(__FILE__).DS.'helper.php');
+$modulePath = JURI::root(true).DS.'modules'.DS.'mod_instagallery'.DS;
+$Instagram = modInstagramHelper::getInstagramObject($params);
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+require JModuleHelper::getLayoutPath('mod_instagallery','default');
+?>
